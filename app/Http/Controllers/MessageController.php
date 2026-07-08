48518ae5042e22dfc02ddf427557cc36e351da77
Wei (wei080781-hash) <wei080781@gmail.com>
2026-07-08 17:29:52 +0800

    引入use App\Events\MessageStatusUpdated;

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use App\Models\Message;
 use App\Jobs\CompressVideoJob;
+use App\Events\MessageStatusUpdated;
 
 class MessageController extends Controller
 {
@@ -106,7 +107,7 @@ class MessageController extends Controller
 
         // 2. 加上這一行：把訊息丟進大聲公廣播出去！
         // .toOthers() 很重要，它能確保「發文者自己」不會重複收到這則推播
-        broadcast(new MessageStatusUpdated($message));
+        broadcast(new MessageStatusUpdated($message))->toOthers();
 
         // 更新物化路徑 (Materialized Path)
         $paddedId = str_pad($message->id, 10, '0', STR_PAD_LEFT);
