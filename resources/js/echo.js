b00ddd1cd36f15cf1f0397cf17f6be5b3905d7ff
Wei (wei080781-hash) <wei080781@gmail.com>
2026-07-13 00:13:42 +0800

    改動echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,8 +7,8 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8081,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8081,
     forceTLS: false,
     enabledTransports: ['ws', 'wss'],
 });
