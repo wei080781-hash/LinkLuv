551f2572f2c86928c2cd224a9815353335ce90a7
Wei (wei080781-hash) <wei080781@gmail.com>
2026-07-29 15:32:59 +0800

    這裡新增判斷來隱藏介面

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -12,12 +12,13 @@
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
-
+            @if (!auth()->user()->google_id)
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
+            @endif
             <!-- 新增帳號google帳號判斷去隱藏刪除帳號外框 -->
             @if (!auth()->user()->google_id)
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
