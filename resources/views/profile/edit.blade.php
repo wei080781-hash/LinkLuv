19324a3391fbb258cb466509d884d2cc65fdb289
Wei (wei080781-hash) <wei080781@gmail.com>
2026-09-03 18:50:49 +0800

    修改個人主頁編輯

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('個人主頁') }}
         </h2>
     </x-slot>
 
@@ -12,21 +12,18 @@
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
-            @if (!auth()->user()->google_id)
+        
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
-            @endif
-            <!-- 新增帳號google帳號判斷去隱藏刪除帳號外框 -->
-            @if (!auth()->user()->google_id)
+           
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
-             @endif
         </div>
     </div>
 </x-app-layout>
