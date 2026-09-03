35901d9d270524f074fa032e5d551669da1c2939
Wei (wei080781-hash) <wei080781@gmail.com>
2026-09-03 19:00:21 +0800

    在個人主頁新增單引號閉合

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('個人資料) }}
+            {{ __('個人資料') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
