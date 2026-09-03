32eb0929f9499247303dbff5698dc6971326b422
Wei (wei080781-hash) <wei080781@gmail.com>
2026-09-03 18:55:40 +0800

    更換主頁文字

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+            {{ __('個人資料) }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("更換大頭貼跟E-mail") }}
         </p>
     </header>
 
