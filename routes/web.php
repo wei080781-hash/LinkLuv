75d7d251144c95036f943a295fd1a97f59150196
Wei (wei080781-hash) <wei080781@gmail.com>
2026-09-08 16:53:40 +0800

    把api/message 路徑命名不一樣以免發生衝突

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::delete('/profile/photo', [ProfileController::class, 'deletePhoto'])->name('profile.photo.delete');
     // 🎯 修正這裡：路徑加上 /api/ 才能跟前端的 fetch 完美對上！
-    Route::get('/api/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('/api/messages', [MessageController::class, 'index'])->name('api.messages.index');
 
     // ✅ 加入 GET /messages
     Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
