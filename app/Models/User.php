45240258f1788397e30fae4b52631ae05341e490
Wei (wei080781-hash) <wei080781@gmail.com>
2026-09-16 16:42:44 +0800

    fix(auth): 將 email_verified_at 加入 User fillable 白名單

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var list<string>
      */
-    protected $fillable = ['name', 'email', 'password', 'profile_photo_path','google_id',];
+    protected $fillable = ['name', 'email', 'password', 'profile_photo_path','google_id', 'email_verified_at'];
 
     /**
      * The attributes that should be hidden for serialization.
